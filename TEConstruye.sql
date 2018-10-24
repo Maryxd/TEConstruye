4bc991b8d09ece18198380da42af3393cb888769
Luis José <louisjo81@gmail.com>
2018-10-23 23:47:25 -0600

    Update TEConstruye.sql
    - Se agregaron los triggers

--- TEConstruye.sql
+++ TEConstruye.sql
@@ -1,180 +1,196 @@
+--Creación de la base de datos
 create database TEConstruye
 GO
 USE TEConstruye
 
+----------------------------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------BLOQUE DE CREACIÓN DE TABLAS---------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+--Tabla para almacenar los empleados
 CREATE TABLE Empleados(
 Nombre VARCHAR(50) NOT NULL,
 Apellido1 VARCHAR (50) NOT NULL,
 Apellido2 VARCHAR (50) NOT NULL,
-Cedula INT NOT NULL,
+PK_Cedula INT NOT NULL,
 Telefono VARCHAR (50) NOT NULL,
 Pago_Hora INT NOT NULL,
-PRIMARY KEY (Cedula)
+PRIMARY KEY (PK_Cedula)
 )
 
+--Tabla para almacenar los ingenieros y arquitectos
 CREATE TABLE Ingenieros(
 Especialidad VARCHAR (50) NOT NULL,
-Codigo INT NOT NULL,
-ID_Empleado INT NOT NULL,
-PRIMARY KEY (Codigo),
-FOREIGN KEY (ID_Empleado) REFERENCES Empleados(Cedula)
+PK_Codigo INT NOT NULL,
+FK_ID_Empleado INT NOT NULL,
+PRIMARY KEY (PK_Codigo),
+FOREIGN KEY (FK_ID_Empleado) REFERENCES Empleados(PK_Cedula)
 )
 
+--Tabla para almacenar los clientes
 CREATE TABLE Clientes (
 Nombre VARCHAR (50) NOT NULL,
-Cedula VARCHAR (50) NOT NULL,
+PK_Cedula VARCHAR (50) NOT NULL,
 Telefono VARCHAR (50) NOT NULL,
-PRIMARY KEY (Cedula),
+PRIMARY KEY (PK_Cedula),
 )
 
+--Tabla para almacenar los materiales
 CREATE TABLE Materiales (
-Nombre_Material VARCHAR (50),
-Codigo VARCHAR (50),
-PrecioUnitario  INT,
-PRIMARY KEY (Codigo)
+Nombre_Material VARCHAR (50) NOT NULL,
+PK_Codigo VARCHAR (50) NOT NULL,
+PrecioUnitario  INT NOT NULL,
+PRIMARY KEY (PK_Codigo)
 )
 
-
+--Tabla para almacenar las obras
 CREATE TABLE Obra (
-ID INT NOT NULL,
-ID_Cliente VARCHAR (50) NOT NULL,
+PK_ID INT NOT NULL,
+FK_ID_Cliente VARCHAR (50) NOT NULL,
 Ubicacion VARCHAR (50) NOT NULL,
-PRIMARY KEY (ID),
-FOREIGN KEY (ID_Cliente) REFERENCES Clientes(Cedula)
+PRIMARY KEY (PK_ID),
+FOREIGN KEY (FK_ID_Cliente) REFERENCES Clientes(PK_Cedula)
 )
 
+--Tabla para almacenar las etapas
 CREATE TABLE Etapa(
-IDEtap int identity(1,1),
+PK_IDEtap int identity(1,1),
 Nombre VARCHAR(50) NOT NULL
-PRIMARY KEY (IDEtap)
+PRIMARY KEY (PK_IDEtap)
 )
 
-
+--Tabla intermedia entre etapa y obra 
 CREATE TABLE EtapaXObra(
-ID INT NOT NULL,
-ID_Obra INT NOT NULL,
-ID_Etapa INT NOT NULL,
+PK_ID INT NOT NULL,
+FK_ID_Obra INT NOT NULL,
+FK_ID_Etapa INT NOT NULL,
 Fecha_Inicio DATETIME NOT NULL,
 Fecha_Fin DATETIME NOT NULL,
-PRIMARY KEY (ID),
-FOREIGN KEY (ID_Obra) REFERENCES Obra(ID),
-FOREIGN KEY (ID_Etapa) REFERENCES Etapa(IDEtap)
+PRIMARY KEY (PK_ID),
+FOREIGN KEY (FK_ID_Obra) REFERENCES Obra(PK_ID),
+FOREIGN KEY (FK_ID_Etapa) REFERENCES Etapa(PK_IDEtap)
 )
 
+--Tabla intermedia entre material y etapa
 CREATE TABLE MaterialXEtapa(
-ID_ME INT NOT NULL,
-ID_Material VARCHAR(50) NOT NULL,
-ID_EtapaxObra INT NOT NULL,
+PK_ID_ME INT NOT NULL,
+FK_ID_Material VARCHAR(50) NOT NULL,
+FK_ID_EtapaxObra INT NOT NULL,
 Cantidad INT NOT NULL,
 Precio INT NOT NULL,
 TotalMAT as Cantidad*Precio,
-Primary KEY (ID_ME),
-FOREIGN KEY (ID_Material) REFERENCES Materiales(Codigo),
-FOREIGN KEY (ID_EtapaxObra) REFERENCES EtapaXObra(ID)
+Primary KEY (PK_ID_ME),
+FOREIGN KEY (FK_ID_Material) REFERENCES Materiales(PK_Codigo),
+FOREIGN KEY (FK_ID_EtapaxObra) REFERENCES EtapaXObra(PK_ID)
 )
 
-
-
+--Tabla para relacionar a los empleados con las obras
 CREATE TABLE WORKS_ON(
-ID_Empleado INT NOT NULL,
-ID_Obra INT NOT NULL,
+FK_ID_Empleado INT NOT NULL,
+FK_ID_Obra INT NOT NULL,
 HorasXSemana FLOAT NOT NULL,
 Semana INT NOT NULL,
-FOREIGN KEY (ID_Empleado) REFERENCES Empleados(Cedula),
-FOREIGN KEY (ID_Obra) REFERENCES Obra(ID)
-
+FOREIGN KEY (FK_ID_Empleado) REFERENCES Empleados(PK_Cedula),
+FOREIGN KEY (FK_ID_Obra) REFERENCES Obra(PK_ID)
 )
 
-
-
+--Tabla para recopilar los gastos de una obra
 CREATE TABLE Gastos(
 Numero_Factura INT NOT NULL,
-ID_Obra INT NOT NULL,
-ID_Etapa INT NOT NULL,
+FK_ID_Obra INT NOT NULL,
+FK_ID_Etapa INT NOT NULL,
 Lugar VARCHAR(50) NOT NULL,
 Fecha DATETIME NOT NULL,
 Semana INT NOT NULL,
 Monto INT NOT NULL,
-FOREIGN KEY (ID_Obra) REFERENCES Obra(ID),
-FOREIGN KEY (ID_Etapa) REFERENCES EtapaXObra(ID)
+FOREIGN KEY (FK_ID_Obra) REFERENCES Obra(PK_ID),
+FOREIGN KEY (FK_ID_Etapa) REFERENCES EtapaXObra(PK_ID)
 )
-
-/*GO
-CREATE TRIGGER ActualizarPresupuestoporEtapa
-	ON MaterialXEtapa
-	AFTER INSERT
-		AS
-		declare @var int
-		select @var=ID_EtapaxObra from inserted
-		BEGIN
-		UPDATE EtapaXObra
-		Set Total=(SELECT sum(TotalMAT) FROM MaterialXEtapa where ID_EtapaxObra=@var)
-		Where ID=@var
-		END
-
 GO
-CREATE TRIGGER ActualizarPresupuestoxObra
-	ON MaterialXEtapa
-	AFTER INSERT
-	AS
-	declare @var2 int
-	declare @var3 int
-	select @var2=ID_EtapaxObra from inserted
-	select @var3=ID_Obra FROM (MaterialXEtapa INNER JOIN EtapaXObra ON ID_EtapaxObra=ID) WHERE ID=@var2
-	BEGIN
-	UPDATE Obra
-	Set Total_Materiales=(SELECT sum(TotalMAT) FROM (MaterialXEtapa INNER JOIN EtapaXObra ON ID_EtapaxObra=ID) where ID_Obra=@var3)
-	where ID=@var3
-	END*/
 
+----------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------BLOQUE DE CREACIÓN DE STORED PROCEDURES------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------
 
-/*DROP TRIGGER ActualizarPresupuestoporObra
-DROP TRIGGER ActualizarPresupuestoporEtapa
-SELECT * FROM MaterialXEtapa*/
-GO
-CREATE PROCEDURE Presupuesto  @IDObra INT
+--Stored Procedure que genera un presupuesto
+CREATE PROCEDURE USP_Presupuesto  @IDObra INT
 	AS
 	SELECT Nombre, Nombre_Material,Cantidad,Precio,TotalMAT
-	FROM MaterialXEtapa LEFT JOIN EtapaXObra ON ID_EtapaxObra=ID LEFT JOIN Etapa ON IDEtap=ID_Etapa LEFT JOIN Materiales ON ID_Material=Codigo/* (EtapaXObra INNER JOIN (MaterialXEtapa INNER JOIN Materiales ON Codigo=ID_Material) ON ID_Etapa=ID_EtapaxObra)*/ 
-	WHERE ID_Obra=@IDObra
+	FROM MaterialXEtapa LEFT JOIN EtapaXObra ON FK_ID_EtapaxObra=PK_ID LEFT JOIN Etapa ON PK_IDEtap=FK_ID_Etapa LEFT JOIN Materiales ON FK_ID_Material=PK_Codigo/* (EtapaXObra INNER JOIN (MaterialXEtapa INNER JOIN Materiales ON Codigo=ID_Material) ON ID_Etapa=ID_EtapaxObra)*/ 
+	WHERE FK_ID_Obra=@IDObra
 	Group by Nombre, Nombre_Material,Cantidad,Precio,TotalMAT
 	GO
-	
-CREATE PROCEDURE Planilla @semana INT
+
+--Stored Procedure que genera una planilla
+CREATE PROCEDURE USP_Planilla @semana INT
 	AS
-	SELECT  Nombre, Apellido1, Apellido2, ID as ID_Obra, Ubicacion, SUM(HorasXSemana*Pago_Hora) as Sueldo
-	FROM WORKS_ON INNER JOIN Obra ON ID_Obra=ID LEFT JOIN Empleados ON ID_Empleado=Cedula
+	SELECT  Nombre, Apellido1, Apellido2, PK_ID as ID_Obra, Ubicacion, SUM(HorasXSemana*Pago_Hora) as Sueldo
+	FROM WORKS_ON INNER JOIN Obra ON FK_ID_Obra=PK_ID LEFT JOIN Empleados ON FK_ID_Empleado=PK_Cedula
 	Where Semana=@semana
-	Group by Nombre, Apellido1, Apellido2, ID, Ubicacion
+	Group by Nombre, Apellido1, Apellido2, PK_ID, Ubicacion
 	GO
 
-CREATE PROCEDURE Gasto @semana INT, @IDObra INT
+--Stored Procedure que genera recuento de gastos
+CREATE PROCEDURE USP_Gasto @semana INT, @IDObra INT
 	AS
 	SELECT Numero_Factura,Lugar,Fecha, Nombre as Etapa, Monto
-	FROM Gastos INNER JOIN EtapaXObra ON Gastos.ID_Etapa=ID LEFT JOIN Etapa ON EtapaXObra.ID_Etapa=IDEtap
-	WHERE Semana=@semana AND Gastos.ID_Obra=@IDObra
+	FROM Gastos INNER JOIN EtapaXObra ON Gastos.FK_ID_Etapa=PK_ID LEFT JOIN Etapa ON EtapaXObra.FK_ID_Etapa=PK_IDEtap
+	WHERE Semana=@semana AND Gastos.FK_ID_Obra=@IDObra
 	GO
 
-
-CREATE PROCEDURE Reporte_de_Estado @IDObra INT
+--Stored Procedure que genera un reporte de estado
+CREATE PROCEDURE USP_Reporte_de_Estado @IDObra INT
 	AS
 	declare @presupuesto int
 	Select @presupuesto=SUM(TotalMAT)
-	FROM MaterialXEtapa LEFT JOIN EtapaXObra ON ID_EtapaxObra=ID LEFT JOIN Etapa ON IDEtap=ID_Etapa LEFT JOIN Materiales ON ID_Material=Codigo/* (EtapaXObra INNER JOIN (MaterialXEtapa INNER JOIN Materiales ON Codigo=ID_Material) ON ID_Etapa=ID_EtapaxObra)*/ 
-	WHERE ID_Obra=@IDObra
+	FROM MaterialXEtapa LEFT JOIN EtapaXObra ON FK_ID_EtapaxObra=PK_ID LEFT JOIN Etapa ON PK_IDEtap=FK_ID_Etapa LEFT JOIN Materiales ON FK_ID_Material=PK_Codigo/* (EtapaXObra INNER JOIN (MaterialXEtapa INNER JOIN Materiales ON Codigo=ID_Material) ON ID_Etapa=ID_EtapaxObra)*/ 
+	WHERE FK_ID_Obra=@IDObra
 
 	SELECT Nombre as Etapa,@presupuesto as Presupuesto,SUM(Monto) as Real,@presupuesto-SUM(Monto) as Diferencia
-	FROM Gastos INNER JOIN EtapaXObra ON Gastos.ID_Etapa=ID LEFT JOIN Etapa ON EtapaXObra.ID_Etapa=IDEtap
-	Where Gastos.ID_Obra=630
+	FROM Gastos INNER JOIN EtapaXObra ON Gastos.FK_ID_Etapa=PK_ID LEFT JOIN Etapa ON EtapaXObra.FK_ID_Etapa=PK_IDEtap
+	Where Gastos.FK_ID_Obra=630
 	GROUP BY Nombre
 	GO
 
+----------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------BLOQUE DE CREACIÓN DE TRIGGERS---------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------
 
-EXEC Planilla 1;
-EXEC Presupuesto 630;
-EXEC Reporte_de_Estado 630;   
+--Trigger que evita que se realicen reducciones en los salarios
+CREATE TRIGGER TR_EvitarReduccionSalario ON Empleados
+FOR UPDATE 
+AS
+	IF UPDATE(Pago_Hora) 
+	BEGIN
+		DECLARE @pago_Anterior int
+		DECLARE @pago_Nuevo int
+		SELECT @pago_Anterior = Pago_Hora FROM deleted
+		SELECT @pago_Nuevo = Pago_Hora FROM inserted
+		IF (@pago_Nuevo < @pago_Anterior)
+		RAISERROR(15600,-1,-1,'Cambio no autorizado: los pagos no pueden ser reducidos')
+		ROLLBACK
+	END
+GO
 
-SELECT * FROM Gastos
+--Trigger que evita que se reporte un material cuyo costo sea cero
+CREATE TRIGGER TR_VerificarPrecioMaterial ON Materiales
+FOR UPDATE 
+AS
+	IF UPDATE(PrecioUnitario) 
+	BEGIN
+		DECLARE @precio_Anterior int
+		DECLARE @precio_Nuevo int
+		SELECT @precio_Anterior = PrecioUnitario FROM deleted
+		SELECT @precio_Nuevo = PrecioUnitario FROM inserted
+		IF (@precio_Nuevo = 0)
+		RAISERROR(15600,-1,-1,'Cambio no autorizado: los materiales deben tener un precio')
+		ROLLBACK
+	END
+GO
 
-	SELECT * FROM EtapaXObra
-	SELECT * FROM MaterialXEtapa
+----------------------------------------------------------------------------------------------------------------------------------------------
+EXEC USP_Planilla 1;
+EXEC USP_Presupuesto 630;
+EXEC USP_Reporte_de_Estado 630;
+
+----------------------------------------------------------ÚLTIMA LÍNEA------------------------------------------------------------------------
